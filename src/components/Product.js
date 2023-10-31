87718440ad01f6a851008c6f625240053c28ddaa
Chae Roy <chaeroy55@yahoo.com>
2023-10-30 21:19:55 -0500

    initial product creation

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ products, category, heading }) => {
+  const filteredProducts = products.filter(item => item.category === category);
+  console.log(filteredProducts)
+  return (
+  <>
+  <h2>{heading}</h2>
+  <div className='grid md:grid-cols-3 lg:grid-cols-5 gap-6'>
+    {filteredProducts.map(product => (
+      <div className='w-full h-[300px] bg-orange-200 mb-4' key={product.id}>
+      <img src={product.image} />
+      <p>{product.title}</p>
+      <p>{product.price}</p>
+    </div>
+    ))}
+  </div>
+  </>
+  );
 };
 
 export default Product;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+import Product from '../components/Product';
 
 const Home = () => {
-  return <div>Homepage</div>;
+  const { products } = useContext(ProductContext);
+
+  const mensProducts = products.filter(item => {
+    return item.category === "men's clothing"
+  }); 
+
+  const womensProducts = products.filter( item => {
+    return item.category === "women's clothing"
+  });
+
+  return <section>
+    <div className='container mx-auto bg-gray-300'>
+      <Product products={products} category={"men's clothing"} heading={"Men's"} />
+    </div>
+  </section>;
 };
 
 export default Home;
