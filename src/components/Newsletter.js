8d79e3e8a276e2ce8066b4ca10fc71c0345ab9f7
Chae Roy <chaeroy55@yahoo.com>
2023-11-01 23:12:17 -0500

    update styling

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className='bg-[#f2f2f2] w-full px-10 py-20 text-center'>
+    <div className='bg-[#f2f2f2] mt-10 w-full px-10 py-20 text-center'>
         <p className='uppercase text-2xl'>Get 15% off your first order</p>
         <p className='text-gray-700'>Stay updated with the latest trends, exclusive offers, and exciting news</p>
         <div className='mt-4 md:w-3/4 lg:w-2/4 mx-auto'>
--- src/components/Support.js
+++ src/components/Support.js
@@ -3,18 +3,18 @@ import { BsBag, BsQuestionCircle, BsChat } from 'react-icons/bs'
 
 const Support = () => {
   return (
-    <div className='mt-10 grid grid-cols-1 md:grid-cols-3  md:gap-4 w-full'>
-        <div className='p-10 hover:cursor-pointer hover:border hover:border-black transition text-center flex flex-col items-center'>
+    <div className='mt-10 grid grid-cols-1 px-5 md:grid-cols-3  md:gap-4'>
+        <div className='p-10 hover:cursor-pointer hover:bg-[#f2f2f2] rounded-md transition text-center flex flex-col items-center'>
             <BsBag className='text-4xl mb-3'/>
             <h3 className='font-semibold'>HOW TO SHOP</h3>
             <p className='text-gray-700'>Guide to shopping and placing orders</p>
         </div>
-        <div className='p-10 hover:cursor-pointer hover:border hover:border-black transition  text-center flex flex-col items-center'>
+        <div className='p-10 hover:cursor-pointer hover:bg-[#f2f2f2] rounded-md transition  text-center flex flex-col items-center'>
             <BsQuestionCircle className='text-4xl mb-3'/>
             <h3 className='font-semibold'>FAQS</h3>
             <p className='text-gray-700'>Answering your questions</p>
         </div>
-        <div className='p-10 hover:cursor-pointer hover:border hover:border-black transition  text-center flex flex-col items-center'>
+        <div className='p-10 hover:cursor-pointer hover:bg-[#f2f2f2] rounded-md transition  text-center flex flex-col items-center'>
             <BsChat className='text-4xl mb-3'/>
             <h3 className='font-semibold'>NEED HELP?</h3>
             <p className='text-gray-700'>24/7 customer assistance</p>
