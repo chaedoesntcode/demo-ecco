a71f85f4aef5f3413ed15d447654c3e058e25c7c
Chae Roy <chaeroy55@yahoo.com>
2023-11-07 10:44:03 -0600

    update hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,18 +16,19 @@ const Hero = ({ activeTab }) => {
   };
 
   const scrollToNextSection = () => {
-    const nextSection = document.getElementById('products-slider'); // Replace 'next-section' with the actual ID of the next section
+    const nextSection = document.getElementById('products-slider');
     if (nextSection) {
       nextSection.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return <section>
-    <div className='bg-[#f5f5f5] bg-center bg-cover  flex flex-col justify-end text-white p-2' style={{ ...divStyle, height: 'calc(100vh - 128px)' }}>
-      <h2 className='text-4xl uppercase'>Unveiling Timeless Elegance</h2>
-      <Link to={`/${activeTab}`} className='underline' onClick={scrollToNextSection}>Shop Now</Link>
+    <div className='bg-[#f5f5f5] bg-center bg-cover flex justify-start items-end px-5 lg:px-10' style={{ ...divStyle, height: 'calc(100vh - 128px)' }}>
+        <div className='flex flex-col gap-y-2 text-white justify-centerw-full py-5 xl:w-2/3'>
+            <h2 className='text-3xl font-semibold uppercase'>Unveiling Timeless Elegance</h2>
+            <Link to={`/${activeTab}`} className='underline' onClick={scrollToNextSection}>Shop Now</Link>
+        </div>  
     </div>
-    
   </section>;
 };
 
