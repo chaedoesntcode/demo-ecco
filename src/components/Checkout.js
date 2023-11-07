50bd3a6ed492bbcf2898f8e00f6676c26bdedc27
Chae Roy <chaeroy55@yahoo.com>
2023-11-07 11:41:24 -0600

    create checkout component

---
+++ src/components/Checkout.js
@@ -0,0 +1,48 @@
+import React, {useContext} from 'react'
+import AmEx from '../img/payments/AmEx.png'
+import ApplePay from '../img/payments/ApplePay.png'
+import Mastercard from '../img/payments/Mastercard.png'
+import Paypal from '../img/payments/Paypal.png'
+import Visa from '../img/payments/Visa.png'
+import Affirm from '../img/payments/Affirm.svg'
+import Klarna from '../img/payments/Klarna.svg'
+import { CartContext } from '../contexts/CartContext'
+
+const Checkout = () => {
+  const { subtotal } = useContext(CartContext)
+
+  return (
+    <>
+    <div className='lg:2/4 xl:w-1/4 flex flex-col gap-y-2 py-4'>
+        <p className='text-2xl mb-4'>Order Summary</p>
+        <div className='flex justify-between text-sm'>
+            <p>Shipping</p>
+            <p>Free</p>
+        </div>
+        <div className='flex justify-between text-sm'>
+            <p>Taxes</p>
+            <p>Calculated at Checkout</p>
+        </div>
+        <div className='flex justify-between font-semibold'>
+            <p>Order Subtotal</p>
+            <p>${subtotal}</p>
+        </div>
+        <button className=' bg-[#282828] w-full my-4 py-4 text-white hover:bg-[#282828]/70 transition uppercase'>Checkout</button>
+        <div className='flex flex-col text-center'>
+            <p className='text-sm text-black/60'>Accepted payment methods</p>
+            <div className='flex justify-center gap-2'>
+                <img src={AmEx} className='h-[25px] rounded-md' />
+                <img src={ApplePay} className='h-[25px] rounded-md' />
+                <img src={Mastercard} className='h-[25px] rounded-md' />
+                <img src={Paypal} className='h-[25px] rounded-md' />
+                <img src={Visa} className='h-[25px] rounded-md' />
+                <img src={Affirm} className='h-[25px] w-[25px] rounded-md' />
+                <img src={Klarna} className='h-[25px] rounded-md' />
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Checkout
--- src/pages/ShoppingBag.js
+++ src/pages/ShoppingBag.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useContext } from 'react'
 import CartItem from '../components/CartItem';
 import { CartContext } from '../contexts/CartContext';
-import AmEx from '..//img/payments/AmEx.png'
-import ApplePay from '..//img/payments/ApplePay.png'
-import Mastercard from '..//img/payments/Mastercard.png'
-import Paypal from '..//img/payments/Paypal.png'
-import Visa from '..//img/payments/Visa.png'
+import Checkout from '../components/Checkout';
+
 
 
 const ShoppingBag = ({setActiveTab}) => {
@@ -24,30 +21,8 @@ const ShoppingBag = ({setActiveTab}) => {
             return <CartItem item={item} key={item.id}/>
           })}
         </div>
-        <div className='lg:2/4 xl:w-1/4 flex flex-col gap-y-2 py-4'>
-          <p className='text-2xl mb-4'>Order Summary</p>
-          <div className='flex justify-between'>
-            <p>Shipping</p>
-            <p>Free</p>
-          </div>
-          <div className='flex justify-between'>
-            <p>Taxes</p>
-            <p>Calculated at Checkout</p>
-          </div>
-          <div className='flex justify-between font-semibold'>
-            <p>Order Subtotal</p>
-            <p>${subtotal}</p>
-          </div>
-          <button className=' bg-[#282828] w-full mt-4 py-4 text-white hover:bg-[#282828]/70 transition uppercase'>Checkout</button>
-          <div className='flex justify-center gap-2'>
-            <img src={AmEx} className='h-[30px] rounded-md' />
-            <img src={ApplePay} className='h-[30px] rounded-md' />
-            <img src={Mastercard} className='h-[30px] rounded-md' />
-            <img src={Paypal} className='h-[30px] rounded-md' />
-            <img src={Visa} className='h-[30px] rounded-md' />
-          </div>
-        
-        </div>
+
+        <Checkout />
       </div>
     
 
